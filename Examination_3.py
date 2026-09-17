e96a391e20f953105672f1cf5f35816a5e9558f2
suwantidanokhong650-glitch <suwantidanokhong650@gmail.com>
2026-09-17 09:57:14 +0700

    Update Examination_3.py

--- Examination_3.py
+++ Examination_3.py
@@ -8,6 +8,8 @@
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
 price = float(input(100))
 is_member = int(input(1))
+
 if is_member = int(input(1))
+if is_member = int(input(0))
 eles :
       print ("ลดทันที 10%")
